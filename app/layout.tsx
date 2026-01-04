4a1afb11eb06f7ef4eb08f888afa62988798efaa
Bill McCoy <mccoy.bill@gmail.com>
2026-01-04 09:31:41 -0800

    Add Ardalio tracking code to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,20 @@ export default function RootLayout({
       <body className="antialiased" suppressHydrationWarning>
         <Analytics />
         {children}
+        {/* Ardalio Tracking Code */}
+        <span id="wts2192966" style={{ display: 'none' }}></span>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              var wts=document.createElement('script');wts.async=true;
+              wts.src='https://app.ardalio.com/wts7.js';document.head.appendChild(wts);
+              wts.onload = function(){ wtsl7(2192966,3); };
+            `,
+          }}
+        />
+        <noscript>
+          <img src="https://app.ardalio.com/7/3/2192966.png" alt="" style={{ display: 'none' }} />
+        </noscript>
       </body>
     </html>
   );
